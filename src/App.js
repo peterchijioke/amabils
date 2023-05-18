c51aa3cd5ef7f9cc075af4e4361b022921f09b63
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-05-18 21:00:29 +0100

    Update: Report

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ReportOne from "./pages/comprehensive/report/one";
+import ReportTwo from "./pages/comprehensive/report/two";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <ReportOne />
+    </div>
+  );
 }
---
+++ src/pages/comprehensive/report/one/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Styles from "./Styles";
+
+export default function index() {
+  return <div>Report 1</div>;
+}
