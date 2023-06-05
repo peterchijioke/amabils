2bda6ce8a4f44bff4385e2b26e0c0c5c1a3aadac
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-05 14:28:07 +0100

    update

--- src/pages/e-station/e-component/top/AppSelect.js
+++ src/pages/e-station/e-component/top/AppSelect.js
@@ -6,32 +6,61 @@ export default function AppSelect({ data = [] }) {
   const mobile = useMediaQuery("(max-width:900px)");
   const [open, setOpen] = useState(false);
   const id = useId();
+  const _handleopen = (setData) => (e) => {
+    setData(!open);
+  };
+  const [selected, setSelected] = useState("Select station");
 
   const data_ = [
-    { id, name: "Goat" },
-    { id, name: "Goat" },
-    { id, name: "Goat" },
+    { id, name: "Ammasco Jabi" },
+    { id, name: "Ammasco Okene" },
+    { id, name: "Ammasco Jabi" },
+    { id, name: "Ammasco Jabi" },
+    { id, name: "Ammasco Kubuwa" },
+    { id, name: "Ammasco Jabi" },
+    { id, name: "Ammasco Lokoja" },
+    { id, name: "Ammasco Jabi" },
   ];
   return (
     <div className="wrap-wrap-select_">
-      <div className="select-wrapper">
-        <div className="selected">{stripeText("peterChijiokeChuke")}</div>
+      <div onClick={_handleopen(setOpen)} className="select-wrapper">
+        <div className="selected">{stripeText(selected, 10)}</div>
         <div className="select-icon-wrap">
-          <ArrowDropDown style={{ fontSize: 40 }} />
+          {open ? <ArrowDropDown /> : <ArrowDropUp />}
         </div>
       </div>
-      <div className="s-dropdown-card"></div>
+      <div
+        style={{
+          display: open && "none",
+        }}
+        className="s-dropdown-card"
+      >
+        {data_.map((item, index) => (
+          <div
+            onClick={() => {
+              setSelected(item.name);
+              setOpen(!open);
+            }}
+            key={index + item.id}
+            className="card-inner"
+          >
+            <label>{stripeText(`${item.name}`, 20)}</label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-const stripeText = (data = "") => {
+const stripeText = (data = "", len = 12) => {
   if (data.length == 0) {
     return data;
   } else {
-    if (data.length > 12) {
-      const da = data.slice(0, 12) + "...";
+    if (data.length > len) {
+      const da = data.slice(0, len) + "...";
       return da;
+    } else if (data.length == len || data.length < len) {
+      return data;
     }
   }
 };
