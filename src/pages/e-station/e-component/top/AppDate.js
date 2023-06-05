8749ee58d5567add3eef87956ba91cb4362e2655
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-05 09:51:51 +0100

    update

--- src/pages/e-station/e-component/top/AppDate.js
+++ src/pages/e-station/e-component/top/AppDate.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./top_.scss";
 
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 export default function AppDate() {
-  return <div className="date-wrapper"></div>;
+  const [date, setDate] = useState(new Date().getDate());
+  const handleSetDate = (setState) => (e) => {
+    setState(e.target.value);
+  };
+  return (
+    <div className="date-wrapper">
+      <input
+        className="date-top"
+        style={{ flexGrow: 1, border: "none" }}
+        type="date"
+        onChange={handleSetDate(setDate)}
+        name=""
+        value={`${date}`}
+      />
+    </div>
+  );
 }
